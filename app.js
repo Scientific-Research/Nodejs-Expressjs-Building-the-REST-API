fa5a915a58a196edb7eddc2ccbd5392c99bbd3a1
Maximilian <scientificresearch309@gmail.com>
2023-12-23 23:57:08 +0100

    and now express will forward the request to the placesRoutes, when we have /api/places in the URL

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const placesRoutes = require("./routes/places-routes");
 
 // Middleware
 const app = express();
-app.use(placesRoutes);
+app.use("/api/places", placesRoutes); // => /api/places/...
 
 app.listen(5000, () => {
   console.log("Connected to the server successfully!");
