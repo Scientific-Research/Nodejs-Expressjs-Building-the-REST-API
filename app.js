503b3d7e1821f857eab8deaa1b10932af4fbddbd
Maximilian <scientificresearch309@gmail.com>
2023-12-25 03:00:22 +0100

    Handling Errors for unsupported Routes

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const placesRoutes = require("./routes/places-routes");
-const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
 
 // Middleware
 const app = express();
 app.use(bodyParser.json());
 
 app.use("/api/places", placesRoutes); // => /api/places/...
-// app.use("/api/user", usersRoutes); // => /api/users/...
+
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route.", 404);
+  // return next(error);
+  throw error;
+});
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
