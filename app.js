5b93dde90b7d6bae3f2df2c436a7a521142a60b9
Maximilian <scientificresearch309@gmail.com>
2023-12-27 22:56:41 +0100

    Installing Mongoose & Connecting our Backend to MongoDB

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const placesRoutes = require("./routes/places-routes");
 const usersRoutes = require("./routes/users-routes");
@@ -7,7 +8,8 @@ const HttpError = require("./models/http-error");
 
 // Middleware
 const app = express();
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+app.use(express.json());
 
 app.use("/api/places", placesRoutes); // => /api/places/...
 app.use("/api/users", usersRoutes); // => /api/users/...
@@ -26,6 +28,21 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || "An unknown error ocurred!" });
 });
 
-app.listen(5000, () => {
-  console.log("Connected to the server successfully!");
-});
+const connectDB = async () => {
+  try {
+    // mongoose.connect() does open, close and all other thing...
+    await mongoose.connect(
+      "mongodb+srv://Maximilian:4N22oIntIDURyhVl@cluster0.ki7w2ay.mongodb.net/places?retryWrites=true&w=majority"
+      // { useNewUrlParser: true, useUnifiedTopology: true }
+    );
+    // console.log("Connected to database!");
+    app.listen(5000, () => {
+      console.log("Server started successfully!");
+      // console.log("Connected to Database successfully!");
+    });
+  } catch (error) {
+    console.log("Connection faild!", error);
+  }
+};
+
+connectDB();
